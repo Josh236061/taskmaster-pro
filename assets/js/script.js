105e76a9eff2ce9f55acd2e2785f5993fe3dd0cc
Josh M Thomas <71394743+Josh236061@users.noreply.github.com>
2022-09-06 11:31:01 -0500

    updated 'textarea' & 'p' jQuery elements in 'js' file

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,41 @@ $(".list-group").on("click", "p", function() {
     .addClass("form-control")
     .val(text);
     $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+
 });
+
+$(".list-group").on("blur", "textarea", function() {
+
+  // get the textarea's current value/text
+  var text = $(this)
+  .val()
+  .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+
+});
+
 
 
 // modal was triggered
